e7f1bc2d60f21877079d0672f5b9be6f2bd58775
khj <uj06134@gmail.com>
2025-11-21 14:11:35 +0900

    기능 연동

--- workspace/khj/SaveUs_통합_기능연동_김훈종/src/main/java/com/example/Ex02/controller/TestController.java
+++ workspace/khj/SaveUs_통합_기능연동_김훈종/src/main/java/com/example/Ex02/controller/TestController.java
@@ -1,7 +1,8 @@
 package com.example.Ex02.controller;
 
-import com.ysb.library.dto.TestDto;
-import com.ysb.library.service.TestService;
+
+import com.example.Ex02.dto.TestDto;
+import com.example.Ex02.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ workspace/khj/SaveUs_통합_기능연동_김훈종/src/main/java/com/example/Ex02/dto/ResultDto.java
@@ -0,0 +1,26 @@
+package com.example.Ex02.dto;
+
+import java.util.List;
+
+// 응답으로 돌아온 JSON 객체를 받아오는 역할만 수행
+public class ResultDto {
+    private List<TestDto> items;
+
+    public ResultDto(List<TestDto> items) {
+        this.items = items;
+    }
+
+    public List<TestDto> getItems() {
+        return items;
+    }
+
+    public void setItems(List<TestDto> items) {
+        this.items = items;
+    }
+
+    public void printItems() {
+        for (TestDto item : this.items) {
+            System.out.println(item);
+        }
+    }
+}
