5f53c77f5d607616bf86735f6cfd5202302a9534
me1onsoda <bkoon1725@gmail.com>
2025-11-24 16:27:13 +0900

    AiFoodService 수정

--- workspace/SaveUs_통합_Mysql/src/main/java/com/example/Ex02/Ex01Application.java
+++ workspace/SaveUs_통합_Mysql/src/main/java/com/example/Ex02/Ex01Application.java
@@ -2,9 +2,7 @@ package com.example.Ex02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling //스케줄러 ( 챌린지 성공 체크 )
 @SpringBootApplication
@@ -14,9 +12,4 @@ public class Ex01Application {
 		SpringApplication.run(Ex01Application.class, args);
 	}
 
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
